a49cc9a717b74e3e6825a6bfa2859b1b4f435fee
Trần Minh Thông <106885015+TranMinhThong2001@users.noreply.github.com>
2023-10-05 15:03:53 +0700

    .

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <>
+        <div className='min-w-full bg-black h-16 font-bold text-white text-3xl text py-4 px-4 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%'>
+          logo
+        </div>
+    </>
+  )
+}
